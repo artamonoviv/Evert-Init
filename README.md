310322e57976626d213fda394ba55ef1d3a8165a
Ivan Artamonov <ivan.s.artamonov@gmail.com>
2019-06-18 16:06:22 +0800

    initial import

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Evert::Init - It's new $module
+
+# SYNOPSIS
+
+    use Evert::Init;
+
+# DESCRIPTION
+
+Evert::Init is ...
+
+# LICENSE
+
+Copyright (C) Ivan Artamonov.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Ivan Artamonov <ivan.s.artamonov@gmail.com>
---
+++ lib/Evert/Init.pm
@@ -0,0 +1,39 @@
+package Evert::Init;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Evert::Init - It's new $module
+
+=head1 SYNOPSIS
+
+    use Evert::Init;
+
+=head1 DESCRIPTION
+
+Evert::Init is ...
+
+=head1 LICENSE
+
+Copyright (C) Ivan Artamonov.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Ivan Artamonov E<lt>ivan.s.artamonov@gmail.comE<gt>
+
+=cut
+
